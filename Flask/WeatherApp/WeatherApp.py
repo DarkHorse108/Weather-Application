11b11673bb833bca643328e5ba04873f1fb24661
samxkim <36899727+samxkim@users.noreply.github.com>
2020-07-09 17:18:56 -0700

    Ported existing html/rearranged files to work with flask

--- Flask/WeatherApp/WeatherApp.py
+++ Flask/WeatherApp/WeatherApp.py
@@ -1,26 +1,28 @@
-#Import Flask and the module required to render HTML pages and deal with
-#requests and redirections between pages
+# Import Flask and the module required to render HTML pages and deal with
+# requests and redirection between pages
 from flask import Flask, render_template, request, redirect
 import requests
 
-#Instantiate the Flask Application/Object
+# Instantiate the Flask Application/Object
 WeatherApp = Flask(__name__)
 
 # temp API key
 API_KEY = '5bf5950b4a834db092b5870ac750a47f'
 
-#The code below handles the default URL/Landing page i.e. flipN.oregonstate.edu:PORTNUMBER automatically executes the view/function below
+
+# The code below handles the default URL/Landing page i.e. flipN.oregonstate.edu:PORTNUMBER automatically executes
+# the view/function below
 @WeatherApp.route('/')
 def index():
+    # We render the file home.html contained in the templates folder
+    return render_template('home.html')
 
-	#We render the file home.html contained in the templates folder
-	return render_template('home.html')
 
 # test route
 @WeatherApp.route('/test_template', methods=['GET', 'POST'])
 def test_route():
     if request.method == 'GET':
-        return render_template('test_template.html', city_name = 'PLACEHOLDER')
+        return render_template('test_template.html', city_name='PLACEHOLDER')
 
     elif request.method == 'POST':
         input_location_name = request.form['location-name']
@@ -29,4 +31,9 @@ def test_route():
         response = requests.get(request_url)
         weather_data = response.json()
 
-        return render_template('test_template.html', city_name = weather_data['city_name'])
+        return render_template('test_template.html', city_name=weather_data['city_name'])
+
+
+# To start flask locally
+# if __name__ == '__main__':
+#     WeatherApp.run(debug=True)
