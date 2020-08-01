be3f42f5213a37581d614af27ab656c602ad3e8c
lathanliles <lathanliles@gmail.com>
2020-07-31 20:17:16 -0700

    created simple weather map module for creating 8 square grid points around a center location
    added leaflet map to the home template
    
    added leaflet css
    
    implemented grid point calculation in the Flask app and mapped them without any actual weather information

--- Flask/WeatherApp/WeatherApp.py
+++ Flask/WeatherApp/WeatherApp.py
@@ -53,6 +53,8 @@ def results():
                 forecast_days = APIRequest.generate_formatted_per_day_weather_data(forecast_weather_json)
                 forecast_days = APIRequest.update_current_day_formatted_weather_data(forecast_days, current_weather_json)
                 location = APIRequest.get_api_returned_location_info(forecast_weather_json)
+                city_coordinates = APIRequest.get_city_coordinates(forecast_weather_json)
+                weather_grid_coordinates = WeatherMap.generate_nine_point_grid(city_coordinates, WEATHER_GRID_SPACING)
 
                 # If forecast_days is NOT None, we received a valid/usable information from the API
                 if forecast_days is not None:
