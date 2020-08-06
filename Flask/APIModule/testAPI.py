40c182f6ec67dba5e57bce8f930133be1434c8c9
yuji-zhou <59626799+yuji-zhou@users.noreply.github.com>
2020-08-05 18:05:13 -0700

    Refactor testAPI.py

--- Flask/APIModule/testAPI.py
+++ Flask/APIModule/testAPI.py
@@ -9,71 +9,41 @@ Class to test APIRequest connection
 '''
 class TestAPIRequest(unittest.TestCase):
 
-    def test_valid_request_example_1(self):
-        test_user_weather_request = APIRequest.UserWeatherRequest("Corvalis", 8,"USA", "Oregon")
-        forcast = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_FORECAST)
-        current = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_CURRENT)
-        data = APIRequest.generate_formatted_per_day_weather_data(forcast,current)
-        self.assertNotEqual(data,None)
+    def test_weather_1(self):
+        test_user_weather = APIRequest.UserWeatherRequest("Corvalis", 8,"USA", "Oregon")
+        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
 
-    def test_valid_request_example_2(self):
-        test_user_weather_request = APIRequest.UserWeatherRequest("Seattle", 8,"USA", "Washington")
-        forcast = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_FORECAST)
-        current = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_CURRENT)
-        data = APIRequest.generate_formatted_per_day_weather_data(forcast,current)
-        self.assertNotEqual(data,None)
+    def test_weather_2(self):
+        test_user_weather = APIRequest.UserWeatherRequest("Seattle", 8,"USA", "Washington")
+        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
 
-    def test_valid_request_example_2(self):
-        test_user_weather_request = APIRequest.UserWeatherRequest("Portland", 8,"USA", "Oregon")
-        forcast = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_FORECAST)
-        current = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_CURRENT)
-        data = APIRequest.generate_formatted_per_day_weather_data(forcast,current)
-        self.assertNotEqual(data,None)
+    def test_weather_3(self):
+        test_user_weather = APIRequest.UserWeatherRequest("Portland", 8,"USA", "Oregon")
+        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
 
-    def test_valid_request_example_4(self):
-        test_user_weather_request = APIRequest.UserWeatherRequest("Los Angeles", 8,"USA", "California")
-        forcast = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_FORECAST)
-        current = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_CURRENT)
-        data = APIRequest.generate_formatted_per_day_weather_data(forcast,current)
-        self.assertNotEqual(data,None)
+    def test_weather_4(self):
+        test_user_weather = APIRequest.UserWeatherRequest("Los Angeles", 8,"USA", "California")
+        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
 
-    def test_valid_request_example_5(self):
-        test_user_weather_request = APIRequest.UserWeatherRequest("San Diego", 8,"USA", "California")
-        forcast = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_FORECAST)
-        current = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_CURRENT)
-        data = APIRequest.generate_formatted_per_day_weather_data(forcast,current)
-        self.assertNotEqual(data,None)
+    def test_weather_5(self):
+        test_user_weather = APIRequest.UserWeatherRequest("San Diego", 8,"USA", "California")
+        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
 
-    def test_valid_request_example_6(self):
-        test_user_weather_request = APIRequest.UserWeatherRequest("Irvine", 8,"USA", "California")
-        forcast = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_FORECAST)
-        current = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_CURRENT)
-        data = APIRequest.generate_formatted_per_day_weather_data(forcast,current)
-        self.assertNotEqual(data,None)
+    def test_weather_6(self):
+        test_user_weather = APIRequest.UserWeatherRequest("Irvine", 8,"USA", "California")
+        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
 
-    def test_valid_request_example_7(self):
-        test_user_weather_request = APIRequest.UserWeatherRequest("New York", 8,"USA", "New York")
-        forcast = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_FORECAST)
-        current = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_CURRENT)
-        data = APIRequest.generate_formatted_per_day_weather_data(forcast,current)
-        self.assertNotEqual(data,None)
+    def test_weather_7(self):
+        test_user_weather = APIRequest.UserWeatherRequest("New York", 8,"USA", "New York")
+        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
 
-    def test_valid_request_example_8(self):
-        test_user_weather_request = APIRequest.UserWeatherRequest("Miami", 8,"USA", "Florida")
-        forcast = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_FORECAST)
-        current = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_CURRENT)
-        data = APIRequest.generate_formatted_per_day_weather_data(forcast,current)
-        self.assertNotEqual(data,None)
+    def test_weather_8(self):
+        test_user_weather = APIRequest.UserWeatherRequest("Miami", 8,"USA", "Florida")
+        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
 
-    def test_valid_request_example_9(self):
-        test_user_weather_request = APIRequest.UserWeatherRequest("Chicago", 8,"USA", "Illinois")
-        forcast = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_FORECAST)
-        current = APIRequest.get_weather_json(test_user_weather_request,API_ENDPOINT_CURRENT)
-        data = APIRequest.generate_formatted_per_day_weather_data(forcast,current)
-        self.assertNotEqual(data,None)
-    
-
-    
+    def test_weather_9(self):
+        test_user_weather = APIRequest.UserWeatherRequest("Chicago", 8,"USA", "Illinois")
+        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
 
 
         
