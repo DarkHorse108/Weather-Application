ccf2be8f91d1f31d3a72bd0cc5d12498f12eccf1
yuji-zhou <59626799+yuji-zhou@users.noreply.github.com>
2020-08-12 21:09:56 -0700

    Update new unit test to use random test all cities

--- Flask/APIModule/testAPI.py
+++ Flask/APIModule/testAPI.py
@@ -3,48 +3,35 @@ import APIRequest
 import random
 from APIRequest import API_ENDPOINT_FORECAST
 from APIRequest import API_ENDPOINT_CURRENT
+import json
+import random
+
 
 '''
 Class to test APIRequest connection
 '''
+
+
+#Sources: https://gist.github.com/ahmu83/38865147cf3727d221941a2ef8c22a77
+
 class TestAPIRequest(unittest.TestCase):
 
-    def test_weather_1(self):
-        test_user_weather = APIRequest.UserWeatherRequest("Corvalis", 8,"USA", "Oregon")
-        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
+    def test_weather(self):
+        #Use random test to test all USA cities
+        f = open("data.json")
+        data = json.load(f)
+        f.close()
+        states = list(data.keys())
 
-    def test_weather_2(self):
-        test_user_weather = APIRequest.UserWeatherRequest("Seattle", 8,"USA", "Washington")
-        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
-
-    def test_weather_3(self):
-        test_user_weather = APIRequest.UserWeatherRequest("Portland", 8,"USA", "Oregon")
-        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
-
-    def test_weather_4(self):
-        test_user_weather = APIRequest.UserWeatherRequest("Los Angeles", 8,"USA", "California")
-        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
-
-    def test_weather_5(self):
-        test_user_weather = APIRequest.UserWeatherRequest("San Diego", 8,"USA", "California")
-        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
-
-    def test_weather_6(self):
-        test_user_weather = APIRequest.UserWeatherRequest("Irvine", 8,"USA", "California")
-        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
-
-    def test_weather_7(self):
-        test_user_weather = APIRequest.UserWeatherRequest("New York", 8,"USA", "New York")
-        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
-
-    def test_weather_8(self):
-        test_user_weather = APIRequest.UserWeatherRequest("Miami", 8,"USA", "Florida")
-        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
-
-    def test_weather_9(self):
-        test_user_weather = APIRequest.UserWeatherRequest("Chicago", 8,"USA", "Illinois")
-        self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
-
+        for i in range(50):
+            state_index = random.randint(0,50)
+            cities = list(data[states[state_index]])
+            test_state = states[state_index]
+            city_index = random.randint(0,(len(cities)-1))
+            test_city = cities[city_index]
+            print("PASSED", test_city, ",",test_state)
+            test_user_weather = APIRequest.UserWeatherRequest(test_city, 8,"USA", test_state)
+            self.assertNotEqual(APIRequest.get_weather(test_user_weather),None)
 
         
         
